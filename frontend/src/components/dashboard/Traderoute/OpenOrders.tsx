b98bb74335c85ab2a6e300ff82c35f91c39871f4
Jineshbansal <732005jinesh@gmail.com>
2025-08-24 03:44:40 +0530

    order book logic implemented

--- frontend/src/components/dashboard/Traderoute/OpenOrders.tsx
+++ frontend/src/components/dashboard/Traderoute/OpenOrders.tsx
@@ -1,6 +1,56 @@
-import React from "react";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
+import { Network, Provider } from "aptos";
+import { useState, useEffect } from "react";
 
+interface Order {
+  lvg: string;
+  qty: string;
+  stock_price: string;
+  user_address: string;
+}
+
+function specificUserTransaction(currArr: Order[], account: any) {
+  console.log(account?.address, "moye");
+  currArr.filter((val) => {
+    val.user_address === account?.address;
+  });
+  console.log("moye moye curr arr", currArr);
+  return currArr;
+}
 const OpenOrders = () => {
+  const { account } = useWallet();
+  const provider = new Provider(Network.DEVNET);
+  const [ask, setAsk] = useState<Order[]>([]);
+  const [bid, setBid] = useState<Order[]>([]);
+  const moduleAddress =
+    "0xc694f211d4385c16fee79540d3276d6b6f407e252f4814c4596831c2405395eb";
+
+  const fetchList = async () => {
+    try {
+      // Assume provider.getAccountResource fetches data from an API
+      const response = await provider.getAccountResource(
+        moduleAddress,
+        `${moduleAddress}::Orderbook::Resource`
+      );
+      const currAsk: Order[] = response.data.asks;
+      const currBid: Order[] = response.data.bids;
+
+      setAsk(specificUserTransaction(currAsk, account));
+      setBid(specificUserTransaction(currBid, account));
+
+      console.log("data :moye moye", currAsk, currBid);
+    } catch (error) {
+      console.log(error, "Error occurred moye!");
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+    const fetchInterval = setInterval(() => {
+      fetchList();
+    }, 500);
+  }, []);
+
   return (
     <>
       <div className="md:flex hidden justify-center items-center h-full w-full border-[0.5px] border-[#383C3F]">
