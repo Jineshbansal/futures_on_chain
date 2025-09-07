89edca7878a5d637384d03590be0083f7bc8780e
Jineshbansal <732005jinesh@gmail.com>
2025-09-08 04:19:37 +0530

    Delete frontend/src/components/dashboard directory

--- frontend/src/components/dashboard/Markets.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import DepthChart from "./Traderoute/DepthChart";
-
-const Markets = () => {
-  return <div></div>;
-};
-
-export default Markets;
--- frontend/src/components/dashboard/Portfolioroute/Order.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Order = () => {
-  return (
-    <div className="flex justify-center items-center h-full w-full">Order</div>
-  );
-};
-
-export default Order;
